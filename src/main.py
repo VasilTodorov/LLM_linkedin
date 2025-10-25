301072be145390d50bf132ff15d43a900dac841d
Васил Тодоров <vasilit@office365student.uni-sofia.bg>
2025-10-26 01:21:13 +0300

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,60 @@
+from google import genai
+from pathlib import Path
+from typing import NamedTuple, Optional
+
+def summarize(text: str) -> str:
+    client = genai.Client(api_key="AIzaSyBbXcPnaht_l7PvbbBiONK12fO--ipArAw")
+    response = client.models.generate_content(  # type: ignore
+        model="gemini-2.5-flash",
+        contents="summarize this text: " + text               
+    )    
+    return response.text # type: ignore
+
+class FileData(NamedTuple):
+        header: str
+        content: str
+
+def read_from_file(path: Path | str)-> Optional[FileData]:    
+    file_path = Path(path)       
+    if file_path.exists() and file_path.is_file(follow_symlinks=False):        
+        with file_path.open('r', encoding='utf-8') as file:
+            content = file.read()
+            header = file_path.name
+            return FileData(header, content) 
+    return None                                      
+    
+def write_to_file(file_path: Path | str, content: str) -> None:
+    path = Path(file_path)
+    path.parent.mkdir(parents=True, exist_ok=True)
+    path.write_text(content, encoding="utf-8")
+    print(f"✅ Written to {path}")
+    
+    
+
+def summarize_files_in_folder(input_folder: str, output_folder: str):
+    input_folder_path = Path(input_folder)
+    output_folder_path = Path(output_folder)
+    #data: List[str] = []
+    if not input_folder_path.exists():
+        print(f"❌ Input folder does not exist: {input_folder}")
+        return
+    
+    output_folder_path.mkdir(parents=True, exist_ok=True)
+
+    for file_path in input_folder_path.iterdir():
+        if file_path.is_file():
+            data = read_from_file(file_path)
+            if not data:
+                print(f"⚠️ Skipping unreadable file: {file_path}")
+                continue
+
+            print(f"🧠 Summarizing: {data.header} ...")
+            summary = summarize(data.content)
+
+            output_path = output_folder_path / data.header
+            write_to_file(output_path, summary)
+
+    print("✨ All files summarized successfully!")
+
+
+summarize_files_in_folder(r"D:\\workspace\\Python\\Training\\LLM_linkedin\\input", r"D:\\workspace\\Python\\Training\\LLM_linkedin\\output")
